896226c21a53d9b261d98e96acb51b0027a2ed47
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-24 21:35:25 +0300

    Update App 2.vue

--- vue-crash-2024/src/App 2.vue
+++ vue-crash-2024/src/App 2.vue
@@ -1,26 +1,42 @@
-<!-- options -->
-<script>
-export default {
-  data() {
-    return {
-      name: 'Lora Mitova',
-      status: 'pending',
-      tasks: ['Task One', 'Task Two', 'Task Tree'],
-      link: 'https://www.google.com/'
-    }
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'pending'
-      } else if (this.status === 'pending') {
-        this.status = 'inactive'
-      } else {
-        this.status = 'active'
-      }
-    }
+<!-- composition -->
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const name = ref('Lora Mitova');
+const status = ref('pending');
+const tasks = ref(['Task One', 'Task Two', 'Task Tree']);
+const newTask = ref('')
+
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending'
+  } else if (status.value === 'pending') {
+    status.value = 'inactive'
+  } else {
+    status.value = 'active'
+  }
+};
+const addTask = () => {
+  if (newTask.value.trim() !== '') {
+    tasks.value.push(newTask.value)
+    newTask.value = ''
   }
 }
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1)
+}
+
+onMounted(async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await res.json()
+    tasks.value = data.map((task) => task.title)
+  }
+  catch (err) {
+    console.log(err);
+
+  }
+})
 </script>
 
 <template>
@@ -30,13 +46,23 @@ export default {
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else="status">User is inactive</p>
 
+  <form @submit.prevent="addTask"
+    style="display: flex; flex-direction: column; align-items:stretch; gap: 10px; margin: 30px 0px;">
+    <label for="newTask" style="font-size: 30px; color: aliceblue;">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks: </h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span style="margin-right: 10px;">
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
-  <!-- <a v-bind:href="link">Click for Google</a> -->
-  <a :href="link">Click for Google</a>
   <br />
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>
@@ -44,8 +70,4 @@ export default {
 
 </template>
 
-<style scoped>
-h1 {
-  color: aqua;
-}
-</style>
+<style></style>
