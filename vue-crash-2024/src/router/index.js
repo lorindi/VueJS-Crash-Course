6fc7cf8b8ff086570a9ab58ee16e8d0bb5f9cee5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-26 00:16:40 +0300

    Add routers

--- vue-crash-2024/src/router/index.js
+++ vue-crash-2024/src/router/index.js
@@ -1,4 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
-import JobListings from './components/JobListings.vue';
+
+import HomeView from "@/views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+  ],
+});
+export default router;
