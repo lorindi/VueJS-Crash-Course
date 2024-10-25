692d98915427eaa78eca4d3cbe8c99bdbbbdef42
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-25 18:52:49 +0300

    Create index.js

---
+++ vue-crash-2024/src/router/index.js
@@ -0,0 +1,4 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
