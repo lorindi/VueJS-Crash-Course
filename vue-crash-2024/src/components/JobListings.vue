4690b27c83af88ed598d59b6295623d744851c3c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-25 18:00:37 +0300

    Update JobListings.vue

--- vue-crash-2024/src/components/JobListings.vue
+++ vue-crash-2024/src/components/JobListings.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, defineProps } from 'vue'
 import JobListing from './JobListing.vue';
 import jobData from '@/jobs.json';
 
 const jobs = ref(jobData.jobs);
+defineProps({
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
+})
 
 </script>
 
@@ -12,9 +19,12 @@ const jobs = ref(jobData.jobs);
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <h1></h1>
-                <JobListing v-for="job in jobs" :key="job.id" :job="job" />
+                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
+    </section>
+    <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            All Jobs</a>
     </section>
 </template>
