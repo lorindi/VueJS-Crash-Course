f55489ed62c1e56aa2343faf16b533bb521b0970
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-27 22:42:42 +0200

    Update JobListings.vue

--- vue-crash-2024/src/components/JobListings.vue
+++ vue-crash-2024/src/components/JobListings.vue
@@ -2,6 +2,7 @@
 import { ref, defineProps } from 'vue'
 import JobListing from './JobListing.vue';
 import jobData from '@/jobs.json';
+import { RouterLink } from 'vue-router';
 
 const jobs = ref(jobData.jobs);
 defineProps({
@@ -24,7 +25,7 @@ defineProps({
         </div>
     </section>
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
-            All Jobs</a>
+        <RouterLink href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View
+            View All Jobs</RouterLink>
     </section>
 </template>
