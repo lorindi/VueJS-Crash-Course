6b9163866151d4586dbeb499e0d04f718463f94e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-29 15:19:11 +0200

    Create BackButton.vue

---
+++ vue-crash-2024/src/components/BackButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import { reactive, ref, defineProps, onMounted } from 'vue'
+import { RouterLink, useRoute } from 'vue-router';
+import axios from 'axios';
+
+
+
+
+
+</script>
+
+<template>
+    <section>
+        <div class="container m-auto py-6 px-6">
+            <RouterLink to="/jobs" class="text-green-500 hover:text-green-600 flex items-center">
+                <i class="pi pi-arrow-circle-left mr-2"></i> Back to Job Listings
+            </RouterLink>
+        </div>
+    </section>
+</template>
